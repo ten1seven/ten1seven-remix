e852d5302284bb33c3cc6085e30bffe9046244f7
Jeremy Fields <jeremy.fields@viget.com>
2022-01-31 18:01:49 -0700

    Add favicon

--- app/root.jsx
+++ app/root.jsx
@@ -18,6 +18,7 @@ export default function App() {
       <head>
         <meta charSet="utf-8" />
         <meta name="viewport" content="width=device-width,initial-scale=1" />
+        <link rel="icon" type="image/png" sizes="32x32" href="/favicon.png" />
         <Meta />
         <Links />
       </head>
